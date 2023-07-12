8a999e8c91fdc9a26f57b72a0999f4fa4cdff9b4
TharaniYasara <tharaniyasara@gmail.com>
2023-07-12 22:42:30 +0530

    project details

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+
+import 'package:flutter/material.dart';
+import 'package:simple_flutter_application/pages/Home_Page.Dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+);
+}
+}
+
+
+
+
+
+
+
+
+
+
+
